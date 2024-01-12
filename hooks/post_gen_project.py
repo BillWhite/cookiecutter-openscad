70a57d3ea8b7e4218a2be82f07be7ab939b7424b
Bill White <bill.white@griggsinst.com>
2024-01-12 15:34:22 -0800

    Make better initial assemblies and source files.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,23 +1,44 @@
-import sys
 import subprocess
 from pathlib import Path
 
+def str2list(strlist) -> list[str]:
+    if not strlist:
+        return []
+    return [t.strip() for t in strlist.split(',') if t.strip() != ""]
+
 def make_assemblies(kind, strlist):
-    names = [t.strip() for t in strlist.split(',') if t.strip() != ""]
+    names = str2list(strlist)
     args = ["bin/make_assembly", f"--{kind}"] + names
     subprocess.run(args)
 
-def make_source_files(strlist):
-    names = [t.strip() for t in strlist.split(',') if t.strip() != ""]
+def make_source_files(strlist, contents=[]):
+    names = str2list(strlist)
     src = Path("src")
     for name in names:
         filepath = src / Path(f'{name}.scad')
         with open(filepath, "w") as of:
-            pass
+            for line in contents:
+                of.write(line + '\n')
+
+def make_one_assembly_definition(namelist) -> list[str]:
+    answer = []
+    for name in namelist:
+        answer += ['module ' + name + ' {', '}']
+    return answer
+
+CONFIG_CONTENTS =['include <GRISCAD/units.scad>',
+                  'include <GRISCAD/vectors.scad>'];
+
 make_assemblies("assembly", "{{cookiecutter.main_assemblies}}")
 make_assemblies("other", "{{cookiecutter.other_assemblies}}")
 make_source_files("{{cookiecutter.other_source_files}}")
+make_source_files("config",
+                  (CONFIG_CONTENTS if ("{{cookiecutter.use_griscad}}" == 'y') else []))
 
-
-
+make_source_files("{{cookiecutter.main_file}}",
+                  ["include <config.scad>"]
+                  + make_one_assembly_definition(str2list("{{cookiecutter.main_assemblies}}"))
+                  + make_one_assembly_definition(str2list("{{cookiecutter.other_assemblies}}")))
+make_source_files("{{cookiecutter.other_source_files}}",
+                  ["include <config.scad>"])
 
